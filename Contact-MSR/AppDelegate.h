6981ff4c98fcc09b41a3989ea96486a48b3863ae
iyinraphael <iyinraphael@gmail.com>
2019-03-13 13:44:07 -0700

    initial commit

---
+++ Contact-MSR/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Contact-MSR
+//
+//  Created by Iyin Raphael on 3/13/19.
+//  Copyright © 2019 Iyin Raphael. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
